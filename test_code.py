99ab981ef533b89b9432c3e2829c229545028af8
Karri Sai Charan <saicharank1397@gmail.com>
2025-05-23 17:00:00 +0530

    third update

--- test_code.py
+++ test_code.py
@@ -4,13 +4,13 @@ class Project1(ABC):
     
     @abstractmethod
     def load_img(self):
-        print("I made some update")
+        print("I made some update--------------1")
 
     @abstractmethod
     def load_model(self):
-        print("I made some update")
+        print("I made some update--------------2")
 
     @abstractmethod
     def model_predict(self):
-        print("I made some update")
+        print("I made some update--------------3")
 
